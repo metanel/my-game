c30f6856391dd157383635957c2687f3db32a999
metanel <malkamatanel@gmail.com>
2025-03-16 16:45:26 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const gameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 400,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: [GameScene]
+};
